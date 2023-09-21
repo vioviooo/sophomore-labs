3de85ad7d0f859f2c5da4c85eff211385e231b94
Victoria Brovina <vika_brovina@mail.ru>
2023-09-21 19:59:32 +0300

    Major bugs fixed

--- 1.c
+++ 1.c
@@ -2,45 +2,80 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <limits.h>
 
-int main(long long argc, char* argv[]) {
+typedef long long ll;
+
+ll factorial(ll n) {
+    ll res = 1;
+    for (ll i = 2; i <= n; i++) {
+        res *= i;
+    }
+    return res;
+}
+
+ll sumOfNums(ll n) {
+    ll sum = 0;
+    for (ll i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+ll lengthOfNum(ll n) {
+    ll len = 0, tmp = llabs(n); // len = legth of the number
+    do {
+        tmp /= 10;
+        len++;
+    } while (tmp != 0);
+
+    return len;
+}
+
+bool isPrime(ll n) {
+    for (ll i = 2; i * i < n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, char* argv[]) {
 
     if (argc != 3) {
         printf("Invalid amount of arguments. Usage: %s <integer> <\"flag\"> \n", argv[0]);
         return 1;
     }
 
-    // !! check for long long overflow
+    for (int i = 0; i < argv[1][i] != '\0'; i++) {
+        if (argv[1][i] < '0' || argv[1][i] > '9') {
+            printf("The first argument is not an integer. Try again.");
+            return 1;
+        }
+    }
 
-    long long numOfCommands = 6;
+    // !! check for integer overflow
+
+    ll numOfCommands = 6;
     char *commands[6] = {"h", "p", "s", "e", "a", "f"};
 
-    // 2 "-p" 2
+    ll num = atoll(argv[1]);
 
-    long long num = atoi(argv[1]);
-
-    // check num
-    // printf("%d \n", num);
-
-    long long ind = -1;
-    for (long long i = 0; i < numOfCommands; i++) {
+    ll ind = -1;
+    for (ll i = 0; i < numOfCommands; i++) {
         if (strcmp(&argv[2][1], commands[i]) == 0) {
-            ind = i;
+            ind = i; // keep the index of the command we need to execute
         }
     }
 
     if (ind == -1) {
-        printf("Invalid command. \n");
+        printf("Invalid command. Try again. \n");
         return 1;
     }
 
-    // char flag[3];
-    // strncpy(flag, argv[2], sizeof(flag));
-
-    // printf("%s \n", flag);
-
     switch (ind) {
-        case 0: 
+        case 0: {
             if (ind == 0) {
                 printf("Can't divide by 0! \n");
                 return 1;
@@ -51,55 +86,43 @@ int main(long long argc, char* argv[]) {
                 return 0;
             }
 
-            for (long long i = 1; i <= 100; i++) {
+            for (ll i = 1; i <= 100; i++) {
                 if (i % num == 0) {
-                    printf("%d", i);
+                    printf("%lld", i);
                 }
-            }
-
-            break;
-        case 1: {
-
-            if (num == 1 || num == 0 || num < 0) {
-                printf("Neither prime nor composite. \n");
-                return 0;
-            }
-
-            bool check = true;
-            for (long long i = 2; i * i < num && check; i++) {
-                if (num % i == 0) {
-                    check = false;
-                }
-            }
-
-            if (check) {
-                printf("Composite.");
-            }
-            else {
-                printf("Prime.");
             }
 
             break;
         }
-        case 2: {
-            if (num < 0) {
-                printf("wtfffff \n");
+        case 1: {
+            if (num == 1 || num == 0 || num < 0) {
+                printf("Neither prime, nor composite. \n");
                 return 0;
             }
+
+            bool check = isPrime(num);
+
+            if (check) {
+                printf("Prime.");
+            }
+            else {
+                printf("Composite.");
+            }
+
+            break;
+        }
+
+        case 2: {
             
-            long long len = 0, tmp = num; // len = legth of the number
-            do {
-                tmp /= 10;
-                len++;
-            } while (tmp != 0);
+            ll len = lengthOfNum(num);
             
             char *arr = (char*)malloc(len + 1); // allocating memory for an array that will store number's digits
             if (arr == (void*)0) {
                 printf("Memory allocation failed! \n");
                 return 1;
             }
-            long long i = 0;
-            tmp = num;
+
+            ll i = 0, tmp = num;
 
             do { // getting digits and putting them into our array
                 arr[i] = tmp % 10;
@@ -107,7 +130,7 @@ int main(long long argc, char* argv[]) {
                 i++;
             } while (tmp != 0);
 
-            long long end = len - 1; // index of the last digit in the array
+            ll end = len - 1; // index of the last digit in the array
             for (i = 0; i <= len / 2 && end >= i; i++, end--) {
                 tmp = arr[i];
                 arr[i] = arr[end];
@@ -122,29 +145,30 @@ int main(long long argc, char* argv[]) {
 
             break;
         }
-        case 3:
+
+        case 3: {
             if (num > 10 || num < 0) {
                 printf("Entered number is out of range! \n");
                 return 1;
             }
-            for (long long i = 1; i <= 10; i++) {
-                long long res = i; // result for each base for each power
+            for (ll i = 1; i <= 10; i++) {
+                ll res = i; // result for each base for each power
                 printf("%lld ", res);
-                for (long long j = 2; j <= num; j++) {
+                for (ll j = 2; j <= num; j++) {
                     res *= res;
                     printf("%lld ", res);
                 }
             }
             break;
+        }
         case 4:
             if (num < 1) {
                 printf("The sum is undefined: entered number is less than 1. \n");
                 return 0;
             }
-            long long sum = 0;
-            for (long long i = 1; i <= num; i++) {
-                sum += i;
-            }
+
+            ll sum = sumOfNums(num);
+
             printf("%lld \n", sum);
 
             break;
@@ -153,11 +177,11 @@ int main(long long argc, char* argv[]) {
                 printf("The factorial of this integer is undefined. The number is less than 0. \n");
                 return 0;
             }
-            long long factorial = 1;
-            for (long long i = 2; i <= num; i++) {
-                factorial *= i;
-            }
-            printf("%s %d %s %lld \n", "The factorial of", num, "is", factorial);
+
+            ll fact = factorial(num);
+
+            printf("%s %lld %s %lld \n", "The factorial of", num, "is", fact);
+
             break;
     }
 
--- other/boo.c
+++ other/boo.c
@@ -3,18 +3,7 @@
 #include <stdbool.h>
 
 int main(int argc, char *argv[]) {
-    int arr[3] = {1, 2, 3};
-
-    int n = 3, end = 3 - 1, tmp;
-    for (int i = 0; i <= n / 2 && end >= i; i++, end--) {
-        tmp = arr[i];
-        arr[i] = arr[end];
-        arr[end] = tmp;
-    }
-
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
+    
 
     return 0;
 }
